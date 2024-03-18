216a2dd948f7e87cf0e5090428b2c3b6f8d02a35
Satyam Pund <pundsatyam@gmail.com>
2024-03-18 17:47:38 +0530

    fetching data with the react router loaders

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,22 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from './../../services/apiRestaurant';
+import MenuItem from './MenuItem.jsx';
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.key} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 import { formatCurrency } from './../../utils/helpers';
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
