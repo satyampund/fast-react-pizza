b7b1f82872a9c2f6877d994635e0310880793f67
Satyam Pund <pundsatyam@gmail.com>
2024-03-29 12:24:27 +0530

    modeling the user state with redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
+
 const Username = () => {
+   const userName =  useSelector(state=>state.user.username)
+
+   if(!userName) return null;
+
     return (
         <div className="text-sm font-semibold hidden md:block">
-            Virat
+           {userName}
         </div>
     );
 };
