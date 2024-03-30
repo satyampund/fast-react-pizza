4bf21b81f60fdeaaab19c266894d47137576b570
Satyam Pund <pundsatyam@gmail.com>
2024-03-30 13:28:31 +0530

    fix: deploy issue-02

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
-import DeleteItem from '../cart/deleteItem';
+import DeleteItem from '../cart/DeleteItem';
 import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
